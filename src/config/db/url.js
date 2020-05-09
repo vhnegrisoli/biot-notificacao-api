7a59b73c50ef967e9de74b97575e03781f26d177
Victor Hugo Negrisoli <victorhugonegrisoli.ccs@hotmail.com>
2020-05-09 10:04:44 -0300

    Adiciona novo usuário do mLab

--- src/config/db/url.js
+++ src/config/db/url.js
@@ -2,4 +2,4 @@ const env = process.env.NODE_ENV;
 export const url =
   env == "development"
     ? "mongodb://localhost:27017/notificacao"
-    : "mongodb://ds141924.mlab.com:41924/heroku_gfbdnnj7";
+    : "mongodb://biotnotificacaoapi:b10tn0t1f1c4c40@ds141924.mlab.com:41924/heroku_gfbdnnj7";
